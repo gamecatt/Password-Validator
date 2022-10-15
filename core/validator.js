5a04a1d5fd0b320d3e9903dc61f417198ae0ed75
gamecatt <kotekotov@gmail.com>
2022-10-15 16:06:03 +0300

    Convert string data from txt file to - JSON items

--- core/validator.js
+++ core/validator.js
@@ -1,5 +1,33 @@
-//init
-document.querySelector('button').addEventListener('click', () =>{
-    let dataFile = document.getElementById('file').files[0];
-    console.log(dataFile);
-});
+let keyDataArray = [];
+
+const getLocalFile = (localFile) => {
+  let reader = new FileReader();
+  reader.readAsText(localFile.files[0]);
+
+  reader.onloadstart = () => {
+    alert("Reader lifecycle - Started!");
+  };
+  reader.onloadend = () => {
+    toJsonArray(reader.result);
+  };
+  reader.onerror = () => {
+    console.log("Reader error : ", reader.error);
+  };
+};
+
+const toJsonArray = (data) => {
+  data.split(/\r\n|\n/).map((line) => {
+    let conditionPasword = line.split(/ |: /);
+    keyDataArray.push({
+      char: conditionPasword[0],
+      range: conditionPasword[1],
+      password: conditionPasword[2],
+    });
+  });
+};
+
+const validate = () => {
+  keyDataArray.map((item) => {
+    console.log(item);
+  });
+};
